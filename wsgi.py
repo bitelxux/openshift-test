75287d9179b895d123abea42416ce5accba628ca
Administrator <admin@example.com>
2017-06-01 11:34:52 +0100

    load

--- wsgi.py
+++ wsgi.py
@@ -11,5 +11,13 @@ def hello():
 def test():
     return "Yipee !! this host is %s" % socket.gethostname()
 
+@application.route("/load")
+def test():
+    html = socket.gethostname()
+    for i in 5000:
+      html += "\n<BR>%d" % i
+
+    return html
+
 if __name__ == "__main__":
     application.run()
